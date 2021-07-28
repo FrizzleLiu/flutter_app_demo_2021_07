8630bd45e165be9c27522f40786ac2162ca3c842
sean <15888897624@163.com>
2021-07-28 16:45:31 +0800

    退出登录

--- lib/article_demo_app/ui/widget/main_drawer.dart
+++ lib/article_demo_app/ui/widget/main_drawer.dart
@@ -92,7 +92,8 @@ class _MainDrawerState extends State<MainDrawer> {
               setState(() {
                 _userName = null;
                 Api.clearCooike();
-                AppManager.prefs?.setString(AppManager.ACCOUNT, _userName!);
+                AppManager.prefs?.setString(AppManager.ACCOUNT, _userName == null ? ""  :_userName!);
+                AppManager.eventBus.fire(LogoutEvent());
               });
             },
             child: const ListTile(
